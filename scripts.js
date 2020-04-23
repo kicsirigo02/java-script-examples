9692701ee1e13c8fd65356bb0f8ecbb8c2c90218
kicsirigo02 <kicsirigo02@gmail.com>
2020-04-23 18:52:48 +0200

    insert first part

---
+++ scripts.js
@@ -0,0 +1,44 @@
+ let name = 'Kriszti';
+
+
+ console.log(name + " says nyenye");
+ console.log('Hello world!')
+
+ // This is a JavaScript comment
+
+ /*
+ This is a multiline 
+ JavaScript comment
+ */
+ 
+ /* 
+name = 'Kriszti'
+"Kriszti"
+age = 26
+26
+oldEnoughToDrive = true
+true
+person = {
+name: 'Kriszti',
+age: 26,
+oldEnoughToDrive: true
+}
+{name: "Kriszti", age: 26, oldEnoughToDrive: true}
+person.name
+"Kriszti"
+person.age +12
+38
+person.name = 'Lajos'
+"Lajos"
+person
+{name: "Lajos", age: 26, oldEnoughToDrive: true}
+age
+26
+oldEnoughToDrive
+true
+person.naem = 'Krinya'
+"Krinya"
+person.favoriteColors = ['yellow', 'Blue']
+(2) ["yellow", "Blue"]
+person
+*/
